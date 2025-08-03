6d1f361214651ce256cedc00c32764525a92d059
Ajaypanchal93 <ajaypanchal4326@gmail.com>
2025-08-03 22:51:54 +0530

    Fix vercel.json routing configuration

--- intern-portal-frontend/src/App.jsx
+++ intern-portal-frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Leaderboard from "./pages/Leaderboard";
+// In App.jsx - temporary test
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
